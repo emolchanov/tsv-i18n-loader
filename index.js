fac2339acdb4a01dcc48f89ac56cbde3adf09772
Eugene Molchanov <box@emolchanov.com>
2015-04-07 02:24:03 +0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*   
- *  locales-loader:
- *  a locales loader from tsv for webpack built 
+ *  tsv-i18n-loader:
+ *  loader for webpack built 
  *  by Eugene Molchanov
  */
 
-var tsvi18n = require("./parsers/tsv-i18n");
+var tsvi18n = new (require("tsv-i18n"));
 
 module.exports = function (text) {
     return 'module.exports = ' + tsvi18n.tsv2obj(text);
